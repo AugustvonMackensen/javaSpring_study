e4ab7e0250c2d2c1106f16e3ecb769d01ee1cf06
AugustvonMackensen <bonaparte_19th@hotmail.com>
2022-10-19 15:50:54 +0900

    공용 pointcut 작성 및 적용해보았음

---
+++ src/main/java/com/test/first/common/aop/BeforeAdviceOutPointcut.java
@@ -0,0 +1,28 @@
+package com.test.first.common.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@Aspect
+public class BeforeAdviceOutPointcut {
+	//따로 작성해 놓은 포인트컷을 가져다가 적용하는 테스트용 클래스임
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Before("CommonPointcut.serviceAllPointcut()")
+	public void beforeConsole(JoinPoint jp) {
+		//모든 *Impl 클래스가 가진 모든 메소드가 구동되기 직전에
+		//실행되는 어드바이스임
+		String methodName = jp.getSignature().getName();
+		Object[] args = jp.getArgs();
+		
+		logger.info("[사전처리] : " + methodName
+				+ "() 메소드 첫번째 전달인자 정보 : "
+				+ ((args.length > 0)? args[0].toString(): 0));
+	}
+}
--- src/main/java/com/test/first/common/aop/CommonPointcut.java
+++ src/main/java/com/test/first/common/aop/CommonPointcut.java
@@ -1,5 +1,22 @@
 package com.test.first.common.aop;
 
-public class CommonPointcut {
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
+@Aspect //aop를 의미함
+public class CommonPointcut {
+	//포인트컷 설정 메소드들만 따로 모아서 작성한 케이스임
+	
+	//포인트컷 설정
+	//execution(접근제어자 패키지명.타겟클래스명.타겟메소드명(파라미터))
+	//패키지.. : 모든 하위 패키지를 의미함
+	//메소드(..): 모든 형식의 매개변수를 의미함
+	@Pointcut("execution(* com.test.first..*Impl.*(..))")
+	public void serviceAllPointcut() {}
+	
+	@Pointcut("execution(* com.test.first..*Impl.select*(..))")
+	public void getPointcut() {}
+	
+	@Pointcut("execution(* com.test.first..*Impl.insert*(..))")
+	public void setPointcut() {}
 }
